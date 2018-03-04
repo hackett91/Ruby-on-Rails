407c02bdde6f842479895e84088fb6d8229cbc26
hackett91 <C16723031@mydit.ie>
2018-03-04 02:00:36 +0000

    adding blogs and migrating tables

--- NewappUdemy/db/schema.rb
+++ NewappUdemy/db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20180301190912) do
 
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
