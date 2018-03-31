9e4693eb099be9470f750c70b3b27d8827c53d6e
hackett91 <C16723031@mydit.ie>
2018-03-31 02:52:16 +0100

    configured custom routes for pages

--- NewappUdemy/config/routes.rb
+++ NewappUdemy/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   resources :portfolios
   resources :blogs
-  get 'pages/home'
 
-  get 'pages/about'
 
-  get 'pages/contact'
 
+  get 'about-me', to: 'pages#about'
+  get 'asdfasdf', to: 'pages#contact'
+
+
+  #settign the root of application to this controller action
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
