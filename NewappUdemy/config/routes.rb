e0d7c92ba25d07a2299a1585e74ac4f7d8b43cd9
hackett91 <C16723031@mydit.ie>
2018-04-01 01:57:31 +0100

    cutomise portfolio routes

--- NewappUdemy/config/routes.rb
+++ NewappUdemy/config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  resources :blogs
+  #passes show into the array
+  resources :portfolios, except: [:show]
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
 
 
-  get 'about-me', to: 'pages#about'
-  get 'asdfasdf', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
 
   #settign the root of application to this controller action
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :blogs
 end
